e4f9edb7d7ab3dbb8e251a0a25bccbbc72216737
VitaliiKyba <k190583@gmail.com>
2020-12-03 16:13:18 +0200

    on finish line

---
+++ main.js
@@ -0,0 +1,17 @@
+/*gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".main",
+      start: "center center",
+      end: "bottom top",
+      markers: true,
+      scrub: true,
+      pin: true,
+    },
+  })
+  .from(".colorhead", { x: innerWidth * -1 })
+  .from(".colordescription", { x: innerWidth * 1 })
+  .from(".gsapimg", {
+    y: innerHeight * 1,
+    rotate: 360,
+  });*/
